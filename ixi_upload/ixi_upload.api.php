b59e066031d61cf28b7a4a14ac030e445a954e3c
Mario Finelli <mfinelli@unleashed-technologies.com>
2014-08-22 13:23:11 -0400

    Update the API documentation.

--- ixi_upload/ixi_upload.api.php
+++ ixi_upload/ixi_upload.api.php
@@ -9,17 +9,28 @@
  * Alter the group node before saving it.
  *
  * @param $node
- *   The new group node.
+ *   This is the new node object that we will be saving.
  *
  * @param $form_state
- *   The submitted form so we can modify based on other data that was part of
- *   the form.
+ *   This is the submitted form_state so that you can access other fields if
+ *   needed.
  */
 function hook_ixi_upload_group_node_alter(&$node, $form_state) {
 }
 
 /**
- * Create new node based on xml
+ * Alter the raw XML string before it's saved to the database.
+ *
+ * @param $xml
+ *   This is the raw xml string, not an object. You can convert it if you
+ *   need to, but it must be converted back before the function returns.
+ */
+function hook_ixi_upload_xml_alter(&$xml) {
+}
+
+/**
+ * Process an XML file. You can do whatever you need to here: create a new
+ * node or taxonomy, for example.
  *
  * @param $xml
  *   A SimpleXml object representing the xml.
@@ -41,6 +52,11 @@ function hook_ixi_upload_group_node_alter(&$node, $form_state) {
  * @param $overrides
  *   An array of fields as defined by the developer as keys with overridden
  *   values (only if they have actually been overridden).
+ *
+ * @return mixed
+ *   The return value isn't important (unless there was an error), as it isn't
+ *   used for anything. One option would be to return the nid of the new node
+ *   that was created. If there was an error you should return false.
  */
 function hook_ixi_upload_parse_xml_file($xml, $images, $files, $ids, $overrides) {
 }
@@ -50,6 +66,19 @@ function hook_ixi_upload_parse_xml_file($xml, $images, $files, $ids, $overrides)
  *
  * @param $xml
  *   A SimpleXml object representing the the xml.
+ *
+ * @param $overrides
+ *   An array of fields as defined by the developer as keys with overridden
+ *   values (only if they have actually been overridden).
+ *
+ * @return array
+ *   You should return a two dimensional array keyed by fields and values as
+ *   arrays of options. Each field is given a row in the preview fields table,
+ *   and the options array needs to have the following values: 'title' which
+ *   is the human readable field name such as 'Title', 'value' which is the
+ *   current value of the field (you should check the overrides array if
+ *   overrides are enabled for the field), and 'override' which is a boolean
+ *   denoting whether the field can be overridden.
  */
-function hook_ixi_upload_preview_fields($xml) {
+function hook_ixi_upload_preview_fields($xml, $overrides) {
 }
